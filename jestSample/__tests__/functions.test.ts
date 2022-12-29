6f060efc12888c89793efa4097bcf22c7f584485
Kou <hikkappi@yahoo.co.jp>
2022-12-29 16:10:13 +0900

    asyncSumOfArraySometimesZeroのテストコードを記述。

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,22 +1,56 @@
 // todo: ここに単体テストを書いてみましょう！
 
-import { asyncSumOfArray, sumOfArray } from "../functions";
+import {
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+  sumOfArray,
+} from "../functions";
+import { DatabaseMock } from "../util";
 
-test("sumOfArray: total array value to equal.", () => {
-  expect(sumOfArray([1, 2, 3])).toBe(6);
+describe("sumOfArrayのテスト", () => {
+  test("配列の合計値が正しいか。", () => {
+    expect(sumOfArray([1, 2, 3])).toBe(6);
+  });
+
+  test("空配列の場合、例外となるか。", () => {
+    expect(() => sumOfArray([])).toThrow(TypeError);
+  });
 });
 
-test("sumOfArray: throw exception.", () => {
-  expect(() => sumOfArray([])).toThrow(TypeError);
+describe("asyncSumOfArrayのテスト", () => {
+  test("配列の合計値が正しいか。", async () => {
+    expect(await asyncSumOfArray([1, 2, 3])).toBe(6);
+  });
+
+  test("空配列の場合、例外となるか。", async () => {
+    const promise = async (): Promise<void> => {
+      await asyncSumOfArray([]);
+    };
+    await expect(promise).rejects.toThrow(TypeError);
+  });
 });
 
-test("asyncSumOfArray: total array value to equal.", async () => {
-  expect(await asyncSumOfArray([1, 2, 3])).toBe(6);
-});
-
-test("asyncSumOfArray: throw exception.", async () => {
-  const promise = async (): Promise<void> => {
-    await asyncSumOfArray([]);
-  };
-  await expect(promise).rejects.toThrow(TypeError);
+// databaseをMock化
+jest.mock("../util");
+const Database = DatabaseMock as jest.Mock;
+describe("asyncSumOfArraySometimesZeroのテスト", () => {
+  test("配列の合計値が正しいか。", async () => {
+    // TODO mockImplementationOnceとmockImplementationの違いを調べる。
+    Database.mockImplementationOnce(() => {
+      return {
+        save(_: number): void {},
+      };
+    });
+    const database = new Database();
+    expect(await asyncSumOfArraySometimesZero([1, 2, 3], database)).toBe(6);
+  });
+  test("空配列の場合、例外となり0を返すか。", async () => {
+    Database.mockImplementationOnce(() => {
+      return {
+        save(_: number): void {},
+      };
+    });
+    const database = new Database();
+    expect(await asyncSumOfArraySometimesZero([], database)).toBe(0);
+  });
 });
