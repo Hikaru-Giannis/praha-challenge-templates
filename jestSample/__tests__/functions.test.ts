6cdb7760490a423681f43ffa600d40742016dec6
Kou <hikkappi@yahoo.co.jp>
2022-12-29 15:50:34 +0900

    asyncSumOfArrayのテストコードを記述。

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,8 +1,22 @@
 // todo: ここに単体テストを書いてみましょう！
 
-import { sumOfArray } from "../functions";
+import { asyncSumOfArray, sumOfArray } from "../functions";
 
 test("sumOfArray: total array value to equal.", () => {
   expect(sumOfArray([1, 2, 3])).toBe(6);
+});
+
+test("sumOfArray: throw exception.", () => {
   expect(() => sumOfArray([])).toThrow(TypeError);
+});
+
+test("asyncSumOfArray: total array value to equal.", async () => {
+  expect(await asyncSumOfArray([1, 2, 3])).toBe(6);
+});
+
+test("asyncSumOfArray: throw exception.", async () => {
+  const promise = async (): Promise<void> => {
+    await asyncSumOfArray([]);
+  };
+  await expect(promise).rejects.toThrow(TypeError);
 });
