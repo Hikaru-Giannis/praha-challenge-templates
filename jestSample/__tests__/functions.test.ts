9592d28af9a74d3fe8d674ba1895c00d2dbb9537
Kou <hikkappi@yahoo.co.jp>
2022-12-29 15:34:19 +0900

    課題2 : sumOfArrayのテストコードを記述。

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,8 @@
 // todo: ここに単体テストを書いてみましょう！
+
+import { sumOfArray } from "../functions";
+
+test("sumOfArray: total array value to equal.", () => {
+  expect(sumOfArray([1, 2, 3])).toBe(6);
+  expect(() => sumOfArray([])).toThrow(TypeError);
+});
