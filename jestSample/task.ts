88e4b2e1e26f6c243b26e0418bcd7fad0ff519d7
Kou <hikkappi@yahoo.co.jp>
2023-01-01 11:24:45 +0900

    課題4の関数をコミット。

---
+++ jestSample/task.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+// テスト1
+export const numberFormat = (value: number) => {
+  const formatter = new Intl.NumberFormat("jp-JP");
+  return formatter.format(value);
+};
+// テスト2
+export const bubbleSort = (arr: number[]) => {
+  let noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+
+  return arr;
+};
+
+class ApiService {
+  public async fetchUserName(userId: number): Promise<string> {
+    const { data } = await axios
+      .get("https://jsonplaceholder.typicode.com/users/" + userId)
+      .catch((error) => {
+        throw Error("Failed to fetch user.");
+      });
+
+    return data.name;
+  }
+}
+
+// テスト3
+export const fetchUserName = async (
+  userId: number,
+  apiService: ApiService
+): Promise<string> => {
+  try {
+    const userName = await apiService.fetchUserName(userId);
+    return userName;
+  } catch (error) {
+    return "";
+  }
+};
