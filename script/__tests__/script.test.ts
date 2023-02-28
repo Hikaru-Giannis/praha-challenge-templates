650f25b4b15dbc6d8868c68ccca87c59a59fa856
Kou <hikkappi@yahoo.co.jp>
2023-03-01 01:44:11 +0900

    TODOリストと失敗するテストケースを作成。

---
+++ script/__tests__/script.test.ts
@@ -0,0 +1,15 @@
+test("multiply", () => {
+  expect(multiply([3, 10, 3])).toBe(90);
+});
+
+test("add", () => {
+  expect(add([3, 10, 3])).toBe(16);
+});
+
+test("subtract", () => {
+  expect(subtract([10, 3])).toBe(7);
+});
+
+test("divide", () => {
+  expect(divide([100, 10])).toBe(10);
+});
