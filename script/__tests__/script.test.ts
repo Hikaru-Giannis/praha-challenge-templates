ae7aad1a6d4349b2b453810d437b85780abe84a3
Kou <hikkappi@yahoo.co.jp>
2023-03-01 02:22:50 +0900

    31個以上の引数の場合、エラーを発生。

--- script/__tests__/script.test.ts
+++ script/__tests__/script.test.ts
@@ -1,17 +1,41 @@
 import { add, divide, multiply, subtract } from "../script";
 
-test("multiply", () => {
-  expect(multiply([3, 10, 3])).toBe(90);
+describe("multiply", () => {
+  test("3と10と3を渡したら、90が返ってくる。", () => {
+    expect(multiply([3, 10, 3])).toBe(90);
+  });
+  test("引数が31個の場合は、例外を返す。", () => {
+    const args = new Array(31).fill(31);
+    expect(() => multiply(args)).toThrowError("Too many arguments.");
+  });
 });
 
-test("add", () => {
-  expect(add([3, 10, 3])).toBe(16);
+describe("add", () => {
+  test("3と10と3を渡したら、16が返ってくる。", () => {
+    expect(add([3, 10, 3])).toBe(16);
+  });
+  test("引数が31個の場合は、例外を返す。", () => {
+    const args = new Array(31).fill(31);
+    expect(() => add(args)).toThrowError("Too many arguments.");
+  });
 });
 
-test("subtract", () => {
-  expect(subtract([10, 3])).toBe(7);
+describe("subtract", () => {
+  test("10と3を渡したら、7が返ってくる。", () => {
+    expect(subtract([10, 3])).toBe(7);
+  });
+  test("引数が31個の場合は、例外を返す。", () => {
+    const args = new Array(31).fill(31);
+    expect(() => subtract(args)).toThrowError("Too many arguments.");
+  });
 });
 
-test("divide", () => {
-  expect(divide([100, 10])).toBe(10);
+describe("divide", () => {
+  test("100と10を渡したら、10が返ってくる。", () => {
+    expect(divide([100, 10])).toBe(10);
+  });
+  test("引数が31個の場合は、例外を返す。", () => {
+    const args = new Array(31).fill(31);
+    expect(() => divide(args)).toThrowError("Too many arguments.");
+  });
 });
--- script/script.ts
+++ script/script.ts
@@ -1,14 +1,26 @@
 export const multiply = (args: number[]): number => {
+  if (args.length >= 31) {
+    throw new Error("Too many arguments.");
+  }
   return args.reduce((arg, sum) => sum * arg, 1);
 };
 export const add = (args: number[]): number => {
+  if (args.length >= 31) {
+    throw new Error("Too many arguments.");
+  }
   return args.reduce((arg, sum) => sum + arg, 0);
 };
 export const subtract = (args: number[]): number => {
+  if (args.length >= 31) {
+    throw new Error("Too many arguments.");
+  }
   const [initial, ...rests] = args;
   return rests.reduce((rest, sum) => rest - sum, initial);
 };
 export const divide = (args: number[]): number => {
+  if (args.length >= 31) {
+    throw new Error("Too many arguments.");
+  }
   const [initial, ...rests] = args;
   return rests.reduce((rest, sum) => rest / sum, initial);
 };
